1c4ff96702a544f261bde47ebec48e68ba57642b
Vitor <Vitor@VAIO>
2014-06-10 13:34:25 -0300

    Adicionando Validador de CPF

--- TreinamentoHabilitados/src/principal/VerificadorDeCpf.java
+++ TreinamentoHabilitados/src/principal/VerificadorDeCpf.java
@@ -4,7 +4,7 @@ package principal;
  * @author Vitor
  * 
  * Essa classe faz a verificação de todos os CPF's digitados em qualquer um dos forms da aplicação
- * Se o cpf for verdadeiro então retornaremos Verdadeiro do contrario o retorno será Falso.
+ * Se o cpf for verdadeiro então retornaremos Verdadeiro do contrario o retorno sera  Falso... 
  */
 public class VerificadorDeCpf {
     private String cpf;
