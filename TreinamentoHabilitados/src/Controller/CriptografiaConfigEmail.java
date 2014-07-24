9c9b82182cc54fd5aa6fd1e9fee7b5daaa05989a
Guilherme <guiihpr@gmail.com>
2014-07-24 12:46:40 -0300

    Gerando criptografia pelo source

--- TreinamentoHabilitados/src/Controller/CriptografiaConfigEmail.java
+++ TreinamentoHabilitados/src/Controller/CriptografiaConfigEmail.java
@@ -6,11 +6,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -19,11 +22,16 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SealedObject;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import Model.ICrypt;
 import Model.UsuarioEmail;
 
 import com.itextpdf.text.pdf.codec.Base64;
+import com.itextpdf.text.pdf.hyphenation.CharVector;
 
 public class CriptografiaConfigEmail implements ICrypt<UsuarioEmail> {
 
@@ -32,23 +40,9 @@ public class CriptografiaConfigEmail implements ICrypt<UsuarioEmail> {
 
 	public CriptografiaConfigEmail() {
 		try {
-			String nameFile = geraNomeCriptografado("key");
-			File fileKey = new File(getClass().getResource(
-					"/Resources/FilesConfig").getPath()
-					+ "/" + nameFile + ".crkey");
-			if (!fileKey.exists()) {
-				gerarAqruivo();
-			}
-
-			FileInputStream in = new FileInputStream(fileKey);
-			ObjectInputStream os = new ObjectInputStream(in);
-			SecretKey key = (SecretKey) os.readObject();
-			os.close();
-			this.key = key;
-
-		} catch (ClassNotFoundException | IOException
-				| NoSuchAlgorithmException e) {
-
+			gerarAqruivo();
+		} catch (InvalidKeySpecException | NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -79,7 +73,7 @@ public class CriptografiaConfigEmail implements ICrypt<UsuarioEmail> {
 		
 		File dir = new File(pasta+nameFile+nameFolder + ".cr");
 		if (!dir.exists()) {
-			Cipher ci = Cipher.getInstance("DES");
+			Cipher ci = Cipher.getInstance("DESede");
 			ci.init(Cipher.ENCRYPT_MODE, key);
 			SealedObject usuarioConfig = new SealedObject(value, ci);
 			FileOutputStream ou = new FileOutputStream((dir));
@@ -121,7 +115,7 @@ public class CriptografiaConfigEmail implements ICrypt<UsuarioEmail> {
 			// dir = new File(dir.getAbsolutePath() + "/" + nameFolder + ".cr");
 			
 			if (dir.exists()) {
-				Cipher dCi = Cipher.getInstance("DES");
+				Cipher dCi = Cipher.getInstance("DESede");
 				dCi.init(Cipher.DECRYPT_MODE, key);
 					FileInputStream in = new FileInputStream(dir);
 
@@ -140,34 +134,21 @@ public class CriptografiaConfigEmail implements ICrypt<UsuarioEmail> {
 		return user;
 	}
 
-	public void gerarAqruivo() {
-		try {
-			StringBuilder builder = new StringBuilder();
-			algorithm = MessageDigest.getInstance("md5");
-			byte[] messageDigest = algorithm.digest("key".getBytes());
-			StringBuilder hexString = new StringBuilder();
-			for (byte b : messageDigest) {
-				hexString.append(String.format("%02X", 0xFF & b));
-			}
-			String nameFile = hexString.toString();
-			File fileKey = new File(getClass().getResource(
-					"/Resources/FilesConfig").getPath()
-					+ "/" + nameFile + ".crkey");
-			SecretKey chave = KeyGenerator.getInstance("DES").generateKey();
-			FileOutputStream ou = new FileOutputStream(fileKey);
-			ObjectOutputStream os = new ObjectOutputStream(ou);
-			os.writeObject(chave);
-		} catch (IOException e) {
-
-		} catch (NoSuchAlgorithmException e) {
-
-			e.printStackTrace();
-		}
+	public void gerarAqruivo() throws InvalidKeySpecException, NoSuchAlgorithmException, UnsupportedEncodingException {
+		String pass = "@uto03sc0l4G3n3r3t3dK3y!";
+	    byte[] bytes = pass.getBytes("UTF-8");
+	    SecretKey key = new SecretKeySpec(bytes, "DESede");
+	    this.key = key;
 
 	}
 
 	public static void main(String[] args) {
-		new CriptografiaConfigEmail().gerarAqruivo();
+		try {
+			new CriptografiaConfigEmail().gerarAqruivo();
+		} catch (InvalidKeySpecException | NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
